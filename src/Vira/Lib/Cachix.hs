541c5f1afab8242433c23a0d06e8dbcfc7ed42ad
shivaraj-bh <me@shivaraj-bh.in>
2025-05-25 21:19:40 +0530

    feat: Add support for Attic

--- src/Vira/Lib/Cachix.hs
+++ src/Vira/Lib/Cachix.hs
@@ -15,3 +15,17 @@ cachixBin = $(staticWhich "cachix")
 
 cachixPushProcess :: Text -> FilePath -> CreateProcess
 cachixPushProcess cache path = proc cachixBin ["push", "-v", toString cache, path]
+
+{- | Path to the `attic` executable
+
+This should be available in the PATH, thanks to Nix and `which` library.
+-}
+atticBin :: FilePath
+atticBin = $(staticWhich "attic")
+
+atticLoginProcess :: Text -> Text -> Text -> CreateProcess
+atticLoginProcess loginName cacheUrl token =
+  proc atticBin ["login", toString loginName, toString cacheUrl, toString token]
+
+atticPushProcess :: Text -> FilePath -> CreateProcess
+atticPushProcess cache path = proc atticBin ["push", toString cache, path]
