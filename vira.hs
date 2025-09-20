2ff2c083937047859925f08ec465135877957e87
shivaraj-bh <me@shivaraj-bh.in>
2025-09-20 12:04:27 +0530

    Test overloaded-record-update

--- vira.hs
+++ vira.hs
@@ -2,6 +2,7 @@
 \ctx pipeline ->
   let isMain = ctx.branch == "main"
   in pipeline
-    & #signoff % #enable .~ True
-    & #cachix % #enable .~ False
-    & #attic % #enable .~ isMain
+    { signoff.enable = False
+    , cachix.enable = True
+    , attic.enable = isMain
+    }
